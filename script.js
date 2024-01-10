45c5dffd4363c3f7c5c94e3048863594f0898e75
Lucas Carpentier <luccarpentier20@gmail.com>
2024-01-10 17:28:33 +0100

    logique condition if/else pour l'application

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Hello World")
+
+let listDeMots = ["Cachalot","Pétunia","Serviette"];
+let score = 0;
+
+let motUtilisateur = prompt("Entrez le mot: " + listDeMots[0]);
+
+if (motUtilisateur === listDeMots[0]) {
+    console.log("Bravo!");
+    score++;
+} else {
+    console.log("Dommage!");
+}
+
+motUtilisateur = prompt("Entrez le mot: " + listDeMots[1]);
+
+if (motUtilisateur === listDeMots[1]) {
+    console.log("Bravo!");
+    score++;
+} else {       
+    console.log("Dommage!");
+}
+
+motUtilisateur = prompt("Entrez le mot: " + listDeMots[2]);
+
+if (motUtilisateur === listDeMots[2]) {
+    console.log("Bravo!");
+    score++;
+} else {
+    console.log("Dommage!");
+}
+
+console.log("Votre score est de " + score + " sur 3");
+
